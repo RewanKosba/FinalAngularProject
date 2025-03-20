495ec6a6c7782813f8c0218c15910f8825e0ffca
Rewan Kosba <rewankosba58@gmail.com>
2025-03-20 09:24:32 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent , RouterOutlet , NavBarComponent , MoviesComponent ,MoviesDetailsComponent , LoginComponent , RegisterComponent , NotfoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home',component:HomeComponent , title:"home page "},
+  { path: 'Login', component: LoginComponent, title: "login" },
+  {path:'MovieDetails/:id',component:MoviesDetailsComponent , title: "Details"},
+  {path:'Register',component:RegisterComponent , title:  "register"},
+  {path:'**',component:NotfoundComponent},
+
+];
